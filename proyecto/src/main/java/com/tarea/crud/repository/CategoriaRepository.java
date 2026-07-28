33ee57ae2cea286193b077b211fbe49b9556edde
Derek Chavarria <dchavarriam@ucenfotec.ac.cr>
2026-07-28 10:18:47 -0600

    Eliminar carpeta proyecto innecesaria

--- proyecto/src/main/java/com/tarea/crud/repository/CategoriaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.tarea.crud.repository;
-
-import com.tarea.crud.entity.Categoria;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
-}
--- proyecto/src/main/java/com/tarea/crud/repository/ProductoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.tarea.crud.repository;
-
-import com.tarea.crud.entity.Producto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductoRepository extends JpaRepository<Producto, Long> {
-}
